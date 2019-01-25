2242c74f8a63c4a4036e6f7a0dc214039a52afea
Unknown <hendrik.poulsen-nautrup@uibk.ac.at>
2019-01-25 19:40:21 +0100

    Conversion to SciGym
    changed dependencies from gym to scigym
    +LICENSE owner update

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_foo',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies the foo needs
+      install_requires=['scigym']  # And any other dependencies the foo needs
 )
